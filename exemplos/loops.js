a03ec2bacae008175f05229a469d36035950d7c7
douglas-cavalcante <douglascavalcante2803@gmail.com.br>
2023-05-17 20:21:04 -0300

    adiciona exemplo

---
+++ exemplos/loops.js
@@ -0,0 +1,17 @@
+var palavras = ['javascript', 'Java', 'C#', 'Python', 'C', 'PHP']
+
+/* exemplo for
+
+for(var contador = 0; contador <= 3; contador++) {
+    console.log("A melhor linguagem do mundo é " + palavras[contador])
+}
+
+*/
+
+// exemplo forEach
+
+palavras.forEach(function(palavra){
+        console.log(palavra)
+     
+        
+})
